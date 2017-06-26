2428ae3b3be872e1ea9850fe492c7e661f06ebd0
SD Davis <davis@devcamp.com>
2017-06-26 14:57:47 +0000

    Intigrated index

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+    Blog.create!(
+        title: "My repeat post #{blog}",
+        
+        body: "repeat bost body #{blog} jfkdajkfd;la fdsjhfjsakhdfsa fdshakj
+        dhsjds fdsfsd fds fsfdsfdsfsd  werw rewrew  fsd c sddcsdcsdfsf"
+        
+        )
+    
+end
+
+5.times do |skill|
+    Skill.create!(
+        title: "Rails #{skill}",
+        percent_utilized: 15
+        )
+    end
+    
+    puts "5 skills created"
+    
+    9.times do |portfolio_item|
+        Portfolio.create!(
+            title: "Portfolio Title",
+            subtitle: "service",
+            body: "derrekbitner:~/workspace $ cd DevCamp/
+                    derrekbitner:~/workspace/DevCamp (master) $ git checkout -b portfollio-feature
+                    Switched to a new branch 'portfollio-feature'
+                    derrekbitner:~/workspace/DevCamp (portfollio-feature) $ rails c
+                    Running via Spring preloader in process 1801
+                    Loading development environment (Rails 5.1.1)
+                    2.4.0 :001 > Blog.create!(title: 'console test', body: 'testing'
+                    2.4.0 :002?>   
+                    SyntaxError: (irb):1: syntax error, unexpected end-of-input, expecting ')'
+                    
+                    derrekbitner:~/workspace/DevCamp (portfollio-feature) $ ",
+
+            main_image: "http://via.placeholder.com/600x400",
+            thumb_image: "http://via.placeholder.com/350x200"
+            )
+            
+        end
