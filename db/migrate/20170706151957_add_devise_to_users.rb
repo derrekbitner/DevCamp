7ce15a2685d2daca76e2719aca8d1b102176bd01
SD Davis <davis@devcamp.com>
2017-07-18 17:31:44 +0000

    created styles and contact page

--- db/migrate/20170706151957_add_devise_to_users.rb
+++ db/migrate/20170706151957_add_devise_to_users.rb
@@ -6,7 +6,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.1]
       t.string :encrypted_password, null: false, default: ""
       
       ## Custom fields
-      t.string :fname
+      ##t.string :fname
 
       ## Recoverable
       t.string   :reset_password_token
